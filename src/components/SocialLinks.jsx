706189b8b73b8435ee451ef67d699070c313ac5e
Akhil Satish <akhilsatish2008@gmail.com>
2022-09-22 12:32:11 -0700

    fixed social link

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -13,7 +13,7 @@ const SocialLinks = () => {
               LinkedIn <FaLinkedin size={30} />
             </>
           ),
-          href: "www.linkedin.com/in/akhil-satish-codebender",
+          href: "https://linkedin.com/in/akhil-satish-codebender",
           style: "rounded-tr-md",
         },
         {
